bd7842f39fb77119549c45c60a1f5cb476cad3fa
Valentin Beaumont <57375180+healkeiser@users.noreply.github.com>
2023-08-22 18:59:53 +0100

    [UPDATE] Pip preparation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyside2_vfx_template",
+    version="0.1.0",
+    description="Custom Python classes and utilities tailored for PySide2 built UI, in VFX-oriented DCC applications.",
+    author="Valentin Beaumont",
+    author_email="valentin.onze@gmail.com",
+    packages=find_packages(),
+)
