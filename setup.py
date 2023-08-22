fe2f2c29aa8e7c0c9ed26dba337a40c7eb42a917
Valentin Beaumont <57375180+healkeiser@users.noreply.github.com>
2023-08-22 19:19:15 +0100

    [UPDATE] Pip setup

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="pyside2_vfx_template",
-    version="0.1.0",
+    version="0.0.2",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     description="Custom Python classes and utilities tailored for PySide2 built UI, in VFX-oriented DCC applications.",
     author="Valentin Beaumont",
     author_email="valentin.onze@gmail.com",
